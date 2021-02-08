2ab25bb3e773572024bfa186f21da8f86675cddc
GuanLuo <41310872+GuanLuo@users.noreply.github.com>
2021-02-08 10:26:21 -0800

    Implement checksum repository agent with MD5sum (#1)
    * Add skeleton for checksum agent
    
    * Implement checksum repository agent with MD5sum
    
    * Address comment. Modify parameters parsing to support multiple files

--- README.md
+++ README.md
@@ -1,4 +1,128 @@
-# Repository Template
+<!--
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-->
 
-Repository template that should be used for all new repositories in
-triton-inference-server.
+[![License](https://img.shields.io/badge/License-BSD3-lightgrey.svg)](https://opensource.org/licenses/BSD-3-Clause)
+
+# Triton Checksum Repository Agent
+
+The repository agent for verifying file checksum before loading the model.
+The repository agent comunicates with Triton using [Triton RepoAgent API](https://github.com/triton-inference-server/core/tree/main/include/triton/core/tritonrepoagent.h).
+Ask questions or report problems in the main Triton [issues
+page](https://github.com/triton-inference-server/server/issues).
+
+## Frequently Asked Questions
+
+Full documentation is included below but these shortcuts can help you
+get started in the right direction.
+
+### Where can I ask general questions about Triton and Triton backends?
+
+Be sure to read all the information below as well as the [general
+Triton
+documentation](https://github.com/triton-inference-server/server#triton-inference-server)
+available in the main
+[server](https://github.com/triton-inference-server/server) repo. If
+you don't find your answer there you can ask questions on the main
+Triton [issues
+page](https://github.com/triton-inference-server/server/issues).
+
+### How do I build the checksum repository agent?
+
+See [build instructions](#build-the-checksum-repository-agent) below.
+
+## Build the Checksum Repository Agent
+
+Use a recent cmake to build. First install the required dependencies.
+
+```
+$ apt-get install openssl-dev
+```
+
+To build the repository agent:
+
+```
+$ mkdir build
+$ cd build
+$ cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install ..
+$ make install
+```
+
+The following required Triton repositories will be pulled and used in
+the build. By default the "main" branch/tag will be used for each repo
+but the listed CMake argument can be used to override.
+
+* triton-inference-server/core: -DTRITON_CORE_REPO_TAG=[tag]
+* triton-inference-server/common: -DTRITON_COMMON_REPO_TAG=[tag]
+
+## Set Up the Checksum Repository Agent
+
+Each repository agent must be implemented as a shared library and the name of
+the shared library must be *libtritonrepoagent_<repo-agent-name>.so*. For
+checksum repository agent, if the name of it is "checksum", a model indicates
+that it uses the repository agent by setting "checksum" as 'name' in agent setting
+inside the model configuration, and Triton looks for
+*libtritonrepoagent_checksum.so* as the shared library that implements
+the checksum repository agent.
+
+For a model, *M* that specifies repository agent *A*, Triton searches for the
+repository agent shared library in the following places, in this order:
+
+* <model_repository>/M/libtritonrepoagent__B.so
+
+* <repository_agent_directory>/libtritonrepoagent__B.so
+
+Where <repository_agent_directory> is by default /opt/tritonserver/repoagents.
+
+The repository agent can be configured by specifying 'parameters' in agent
+setting inside the model configuration. The checksum repository agent will
+examine the parameters in the following way: the key specifies the message
+digest algorithm that is used to generate the checksum and the path to file
+relative to the model repository as <algorithm:relative/path/to/file> and the
+value specifies the expected checksum of the file. For example, if a model uses
+checksum repository agent to verify a file inside the model repository named
+"embedding_table", given the expected MD5 checksum of the file to be
+d726e132f91f16ebee703d96f6f73cb1, the model configuration will be:
+
+```
+model_repository_agents
+{
+  agents [
+    {
+      name: "checksum",
+      parameters
+      {
+        key: "MD5:embedding_table",
+        value: "d726e132f91f16ebee703d96f6f73cb1"
+      }
+    }
+  ]
+}
+```
+
+With the above setup, the checksum repository agent will be invoked before
+loading the model and reject the loading if the checksum does not match.
---
+++ src/checksum.cc
@@ -0,0 +1,230 @@
+// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "triton/core/tritonrepoagent.h"
+#include "triton/core/tritonserver.h"
+
+#include <openssl/md5.h>
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+//
+// Checksum Repository Agent that implements the TRITONREPOAGENT API.
+//
+
+namespace triton { namespace repoagent { namespace checksum {
+
+using RelativeFilePath = std::string;
+using HashString = std::string;
+
+namespace {
+//
+// ErrorException
+//
+// Exception thrown if error occurs while running ChecksumRepoAgent
+//
+struct ErrorException {
+  ErrorException(TRITONSERVER_Error* err) : err_(err) {}
+  TRITONSERVER_Error* err_;
+};
+
+#define THROW_IF_TRITON_ERROR(X)                                    \
+  do {                                                              \
+    TRITONSERVER_Error* tie_err__ = (X);                            \
+    if (tie_err__ != nullptr) {                                     \
+      throw triton::repoagent::checksum::ErrorException(tie_err__); \
+    }                                                               \
+  } while (false)
+
+
+#define THROW_TRITON_ERROR(CODE, MSG)                                 \
+  do {                                                                \
+    TRITONSERVER_Error* tie_err__ = TRITONSERVER_ErrorNew(CODE, MSG); \
+    throw triton::repoagent::checksum::ErrorException(tie_err__);     \
+  } while (false)
+
+
+#define RETURN_IF_ERROR(X)               \
+  do {                                   \
+    TRITONSERVER_Error* rie_err__ = (X); \
+    if (rie_err__ != nullptr) {          \
+      return rie_err__;                  \
+    }                                    \
+  } while (false)
+
+class CheckSum {
+ public:
+  virtual std::string AlgorithmTypeString() = 0;
+  virtual std::string GenerateHash(const std::string& message) = 0;
+};
+
+class MD5Sum : public CheckSum {
+ public:
+  std::string AlgorithmTypeString() override { return "MD5"; }
+
+  std::string GenerateHash(const std::string& message) override
+  {
+    MD5((unsigned char*)message.data(), message.size(), result_);
+    unsigned char low_mask = 0b00001111;
+    unsigned char high_mask = 0b11110000;
+    std::stringstream stream;
+    for (size_t idx = 0; idx < MD5_DIGEST_LENGTH; idx++) {
+      stream << std::hex << (size_t)((result_[idx] & high_mask) >> 4)
+             << std::hex << (size_t)(result_[idx] & low_mask);
+    }
+    return stream.str();
+  }
+
+ private:
+  unsigned char result_[MD5_DIGEST_LENGTH];
+};
+
+std::pair<std::unique_ptr<CheckSum>, RelativeFilePath>
+ParseAlgorithmAndFile(const std::string& algorithm_and_file)
+{
+  auto pos = algorithm_and_file.find(":");
+  if (pos == std::string::npos) {
+    THROW_TRITON_ERROR(
+        TRITONSERVER_ERROR_INVALID_ARG,
+        (std::string("failed to parse key: ") + algorithm_and_file).c_str());
+  }
+
+  std::string lower_key = algorithm_and_file.substr(0, pos);
+  std::transform(
+      lower_key.begin(), lower_key.end(), lower_key.begin(),
+      [](unsigned char c) { return std::tolower(c); });
+
+  std::unique_ptr<CheckSum> hash_utility;
+  if (lower_key == "md5") {
+    hash_utility.reset(new MD5Sum());
+  } else {
+    THROW_TRITON_ERROR(
+        TRITONSERVER_ERROR_UNSUPPORTED,
+        (std::string("Unsupported checksum algorithm: ") +
+         algorithm_and_file.substr(0, pos))
+            .c_str());
+  }
+  return {std::move(hash_utility), algorithm_and_file.substr(pos + 1)};
+}
+
+const std::string
+ReadFile(const std::string& model_dir, const std::string& relative_file_path)
+{
+  // It could be more efficient to generate the checksum by streaming through
+  // the file than reading it in and then digesting it, but the latter is
+  // sufficient as an example.
+  std::ifstream in(
+      model_dir + "/" + relative_file_path, std::ios::in | std::ios::binary);
+  if (!in) {
+    THROW_TRITON_ERROR(
+        TRITONSERVER_ERROR_INVALID_ARG,
+        (std::string("failed to open text file for read ") +
+         relative_file_path + ": " + strerror(errno))
+            .c_str());
+  }
+
+  std::string res;
+  in.seekg(0, std::ios::end);
+  res.resize(in.tellg());
+  in.seekg(0, std::ios::beg);
+  in.read(&res[0], res.size());
+  in.close();
+
+  return res;
+}
+
+}  // namespace
+
+/////////////
+
+extern "C" {
+
+TRITONSERVER_Error*
+TRITONREPOAGENT_ModelAction(
+    TRITONREPOAGENT_Agent* agent, TRITONREPOAGENT_AgentModel* model,
+    const TRITONREPOAGENT_ActionType action_type)
+{
+  // Return success (nullptr) if the agent does not handle the action
+  if (action_type != TRITONREPOAGENT_ACTION_LOAD) {
+    return nullptr;
+  }
+  const char* location_cstr;
+  TRITONREPOAGENT_ArtifactType artifact_type;
+  RETURN_IF_ERROR(TRITONREPOAGENT_ModelRepositoryLocation(
+      agent, model, &artifact_type, &location_cstr));
+  if (artifact_type != TRITONREPOAGENT_ARTIFACT_FILESYSTEM) {
+    return TRITONSERVER_ErrorNew(
+        TRITONSERVER_ERROR_UNSUPPORTED,
+        (std::string("Unsupported filesystem: ") +
+         std::to_string(artifact_type))
+            .c_str());
+  }
+  const std::string location(location_cstr);
+
+  // Check the agent parameters for the checksum configuration of the model
+  uint32_t parameter_count = 0;
+  RETURN_IF_ERROR(
+      TRITONREPOAGENT_ModelParameterCount(agent, model, &parameter_count));
+  for (uint32_t idx = 0; idx < parameter_count; idx++) {
+    const char* key = nullptr;
+    const char* value = nullptr;
+    RETURN_IF_ERROR(
+        TRITONREPOAGENT_ModelParameter(agent, model, idx, &key, &value));
+
+    try {
+      auto algorithm_and_file = ParseAlgorithmAndFile(key);
+      const auto generated_hash = algorithm_and_file.first->GenerateHash(
+          ReadFile(location, algorithm_and_file.second));
+      if (generated_hash != value) {
+        return TRITONSERVER_ErrorNew(
+            TRITONSERVER_ERROR_INVALID_ARG,
+            (std::string("Mismatched ") +
+             algorithm_and_file.first->AlgorithmTypeString() +
+             " hash for file " + algorithm_and_file.second +
+             ", expect: " + value + ", got: " + generated_hash)
+                .c_str());
+      }
+    }
+    catch (const ErrorException& ee) {
+      return ee.err_;
+    }
+  }
+
+  return nullptr;  // success
+}
+
+}  // extern "C"
+
+}}}  // namespace triton::repoagent::checksum
